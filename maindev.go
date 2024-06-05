f54958e8e858d6cc9861daf918dcafb23bb3b963
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2024-06-05 08:08:23 +0530

    05-June-2024

---
+++ maindev.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Print("Peddireddy")
+    fmt.Print("TDP Won The assembly elections")
+}
+// Execution speed
+// Compiled Languages : C,C++,GO,Rust
+// Compilation: main.go --> Go build ---> hell0.exe
+// 
+
+
+
